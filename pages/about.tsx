1bfc5435ec0437a4fb7ae4fa1b2e19100071abcc
Glen Maddern <glenmaddern@gmail.com>
2018-07-09 16:24:35 +1000

    this is pretty neat from the unsplash api - gives you the dominant colour to display while loading

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,21 @@
-import { Outer } from '../components/Outer'
+import { Frame, FrameProps, Outer } from '../src/components/Outer'
 import Link from 'next/link'
+import { getRandomBackground } from '../src/utils/api'
+import React from 'react'
 
-export default () => (
-  <Outer>
-    <span>About us</span>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
-  </Outer>
-)
+export default class extends React.Component<FrameProps> {
+  static getInitialProps = getRandomBackground
+
+  render() {
+    return (
+      <Frame background={this.props.background} color={this.props.color}>
+        <Outer>
+          <span>About us</span>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </Outer>
+      </Frame>
+    )
+  }
+}
